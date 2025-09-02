64136a7e6206e22e5819c74745f9fbf896bef1f0
david x <dx68323@gmail.com>
2025-09-02 16:12:24 +0300

    מתקדם לא סופי 2

--- הצפנה.py
+++ הצפנה.py
@@ -1,27 +1,4 @@
-#
-# class Encryptor:
-#
-#     def __init__(self,key:int):
-#         self.key=key
-#     def encrypt (self,data:str):
-#         encrypted = bytearray()
-#         for byte in data.encode():
-#             encrypted.append(byte^ self.key)
-#             return bytes(encrypted)
-#
-#         def decrypt(self, data):
-#             decrypted = bytearray()
-#             for byte in data:
-#                 decrypted.append(byte ^ self.key)
-#             return decrypted.decode()
-#         #קוד לדוגמא
-# enc = Encryptor(123)
-# text = "Hello World"
-# encrypted = enc.encrypt(text)
-# print("Encrypted:", encrypted)
-#
-# decrypted = enc.decrypt(encrypted)
-# print("Decrypted:", decrypted)
+
 import random
 
 # מפתח אקראי של אורך 8 בתיםv u
